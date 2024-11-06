4ff9de652357a97f78e138a2193c006a798090c3
Aalizelau <aalizelau@gmail.com>
2024-11-06 17:16:56 +0800

    basic code

---
+++ backend/audio_to_text.py
@@ -0,0 +1,16 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+client = OpenAI()
+
+def convert_audio_to_text(local_input_file_path):
+    audio_file= open(local_input_file_path, "rb")
+    transcription = client.audio.transcriptions.create(
+    model="whisper-1", 
+    file=audio_file
+    )
+    print(transcription.text)
+    return transcription.text
+
+transcription_result = convert_audio_to_text("/Users/funlau/Documents/ChatCampus/backend/testing.m4a")
