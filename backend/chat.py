b9729a47125c3667bdfd32cd28afd0aee49fbcf8
Aalizelau <aalizelau@gmail.com>
2024-11-07 17:48:08 +0800

    simpler way to call openAI

--- backend/chat.py
+++ backend/chat.py
@@ -1,34 +1,11 @@
-from langchain.chat_models import ChatOpenAI
-from langchain.memory import ConversationBufferMemory
-from langchain.chains import ConversationChain
-from project_config import setup_app_config
+from langchain.llms import OpenAI
+from dotenv import load_dotenv
+import os
 
-setup_app_config()
+load_dotenv()
+openai_api_key = os.getenv("OPENAI_API_KEY")
 
-llm = ChatOpenAI(temperature=1)
-
-chat_memory = ConversationBufferMemory()
-
-
-def handle_get_response_for_user(user_prompt: str) -> str:
-    conversation = ConversationChain(
-        llm=llm,
-        verbose=False,
-        memory=chat_memory
-    )
-
-    result = conversation.predict(input=user_prompt)
-    print("result >> ", result)
-    return result
-
-
-def run():
-    while True:
-        user_prompt = input("Ask something to the bot, or 0 to leave:")
-        if user_prompt == "0":
-            break
-        handle_get_response_for_user(user_prompt)
-
-
-if __name__ == "__main__":
-    run()
+llm = OpenAI(openai_api_key=openai_api_key, temperature=0.5)
+prompt = "What are the benefits of using LangChain with OpenAI?"
+response = llm(prompt)
+print(response)
