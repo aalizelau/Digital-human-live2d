773ad31952c57de09a9c37aa5c842484ba630d45
Aalizelau <aalizelau@gmail.com>
2024-11-21 17:02:28 +0800

    update how to save audio file

--- backend/audio_helper/text_to_audio.py
+++ backend/audio_helper/text_to_audio.py
@@ -1,18 +1,26 @@
 import os 
 from gtts import gTTS
 
-def convert_text_to_audio(text, language, filename="response.mp3"):
+def convert_text_to_audio(text, language, filename="response_output.mp3"):
     print("Converting response to speech...")
+
+    script_dir = os.path.dirname(__file__)
+    backend_dir = os.path.abspath(os.path.join(script_dir, ".."))
+    audio_folder = os.path.join(backend_dir, "audio")
+    os.makedirs(audio_folder, exist_ok=True)
+    audio_file_path = os.path.join(audio_folder, filename)
+
+    # Generate and save the audio file
     tts = gTTS(text=text, lang=language)
-    # tts = gTTS(text=text, lang='en')
-    tts.save(filename)
-    print(f"Audio saved as {filename}")
-    return filename
+    tts.save(audio_file_path)
+    print(f"Audio saved as {audio_file_path}")
+    return audio_file_path
     
 # if __name__ == "__main__":
 #     # test_text = "Hello, this is a test of the text-to-speech functionality!"
-#     test_text_CN = "妳好，這是一個中文語言的測試"
-#     output_filename = convert_text_to_audio(test_text_CN)
+#     text = "妳好，這是一個中文語言的測試"
+#     language= "zh-CN"
+#     output_filename = convert_text_to_audio(text,language)
     
 #     # Check if the file was created
 #     if os.path.exists(output_filename):
