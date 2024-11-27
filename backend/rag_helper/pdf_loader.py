7b94734aa3b51026ef8adc59c725cb77bd9f06d7
Aalizelau <aalizelau@gmail.com>
2024-11-27 14:47:04 +0800

    change loader

--- backend/rag_helper/pdf_loader.py
+++ backend/rag_helper/pdf_loader.py
@@ -1,26 +1,25 @@
-from PyPDF2 import PdfReader 
+from langchain.document_loaders import PyPDFDirectoryLoader
+import sys
+import os
 
-#process entire pdf
-def get_pdf_text(pdf_docs):
-    text = ""
-    for pdf in pdf_docs:
-        pdf_reader = PdfReader(pdf)
-        for page in pdf_reader.pages:
-            text += page.extract_text()
-    return text 
 
-#process only few pages
-def get_pdf_preview(pdf_docs, max_pages=10):
-    text = ""
-    for pdf in pdf_docs:
-        pdf_reader = PdfReader(pdf)
-        page_count = min(len(pdf_reader.pages), max_pages)
-        for page_num in range(page_count):
-            text += pdf_reader.pages[page_num].extract_text()
-    # print("All PDF pages are processed.")
-    # print(text)
-    return text
+def load_documents():
+	# Add the backend directory to the Python module search path
+    script_dir = os.path.dirname(__file__)
+    backend_dir = os.path.abspath(os.path.join(script_dir, '..'))
+    sys.path.append(backend_dir)
 
-#testing
-# get_pdf_text(["/Users/funlau/Documents/ChatCampus/backend/pdf/KB_fake_news.pdf"])
-# get_pdf_preview(["/Users/funlau/Documents/ChatCampus/backend/pdf/KB_fake_news.pdf"])
+    #KB source
+    script_dir = os.path.dirname(__file__)
+    backend_dir = os.path.abspath(os.path.join(script_dir, '..'))
+    directory_path  = os.path.join(backend_dir, 'PDF')
+    print(f"PDF File Path: {directory_path}")
+    # pdf_path=["/Users/funlau/Documents/ChatCampus/backend/pdf/KB_fake_news.pdf"] 
+    loader = PyPDFDirectoryLoader(directory_path)
+    documents = loader.load()
+    print(documents[1])
+    return documents
+
+#testing 
+load_documents()
+
