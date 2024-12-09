e4aab922eac1fab7430b55b132e723244613a0f0
Aalizelau <aalizelau@gmail.com>
2024-12-09 16:23:50 +0800

    add start up process

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from assistant.assistant_controller import controller as AssistantAudioController
+from assistant.assistant_service import data_pipeline
+from contextlib import asynccontextmanager
 
 app = FastAPI()
 
@@ -9,6 +11,17 @@ origins = [
     "http://localhost:3000",
     "*"
 ]
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("running start up process")
+    await data_pipeline()
+    yield  # This yields control to allow the application to run
+    # Shutdown: Add cleanup code if needed
+    print("Application shutting down...")
+
+# Attach the lifespan handler to the app
+app.router.lifespan_context = lifespan
 
 app.include_router(AssistantAudioController, tags=['assistant'])
 
