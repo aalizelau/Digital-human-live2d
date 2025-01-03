6f25f40bb0197e30ec518c42fc01c71554610627
Aalizelau <aalizelau@gmail.com>
2025-01-02 21:24:03 -0800

    feat: add canvas and model display

--- frontend/src/pages/components/DigitalHumanContainer/DigitalHumanContainer.component.tsx
+++ frontend/src/pages/components/DigitalHumanContainer/DigitalHumanContainer.component.tsx
@@ -10,7 +10,55 @@ import VoiceRecorder from '../VoiceRecorder/VoiceRecorder.component';
 import AudioPlayer from '../AudioPlayer/AudioPlayer.component';
 import ReactLoading from 'react-loading';
 
-const DigitalHumanContainer = () => {
+import { useEffect, useRef } from "react";
+import * as PIXI from 'pixi.js';
+import { Live2DModel } from 'pixi-live2d-display';
+
+declare global {
+  interface Window {
+    PIXI: typeof import('pixi.js');
+  }
+}
+
+let model;
+window.PIXI = PIXI;
+
+const DigitalHumanContainer =() => {
+  const canvasRef = useRef<HTMLCanvasElement | null>(null); // Initialize with null
+
+  useEffect(() => {
+    const setupModel = async () => {
+      const canvas = canvasRef.current; // Access the current canvas
+      if (!canvas) {
+        console.error("Canvas not found");
+        return; // Exit if canvas is still null
+      }
+      const app = new PIXI.Application({
+        view: canvas, // Use the actual canvas element
+        height: 1000,
+        width: 700,
+        autoDensity: true,
+        antialias: true,
+        // resolution: window.devicePixelRatio,
+      });
+      try {
+        // Load the Live2D model
+        const url = "http://localhost:3000/model3/shizuku.model.json"
+        // const url = `${window.location.origin}/shizuku_model/shizuku.model.json`
+        const model = await Live2DModel.from(
+          url
+        );
+        // Add the model to the PIXI stage
+        app.stage.addChild(model);
+        model.scale.set(0.5);
+        model.position.set(-75, -50);
+      } catch (error) {
+        console.error("Failed to load Live2D model:", error);
+      }
+    };   
+    setupModel();
+  }, []);
+
   const {
     handleUserVoiceRecorded,
     isWaitingAIOutput,
@@ -28,6 +76,7 @@ const DigitalHumanContainer = () => {
       <div className="w-48 h-48 md:w-80 md:h-80 rounded-full mb-6 relative overflow-hidden">
         <div className="absolute inset-0 bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 opacity-50 animate-pulse"></div>
         <VoiceAssistantAvatar />
+        <canvas ref={canvasRef}> </canvas>
       </div>
       <VoiceRecorder onAudioRecordingComplete={handleUserVoiceRecorded} />
       {isWaitingAIOutput && (
